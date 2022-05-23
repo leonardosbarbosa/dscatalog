d1ab91fe23dd74b9573a4d7f9f71fd39c04d4dca
Leonardo Barbosa <leonardo.sbarbosa2801@gmail.com>
2022-05-22 21:44:16 -0300

    Revert "delete Category"
    This reverts commit 1258a94022898d82c7fb0b617d8fc75f42ecc387.

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -17,25 +16,12 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(status.value());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(status).body(err);
-	}
-
-	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
-		HttpStatus status = HttpStatus.BAD_REQUEST;
-		StandardError err = new StandardError();
-		err.setTimestamp(Instant.now());
-		err.setStatus(status.value());
-		err.setError("Database exception");
-		err.setMessage(e.getMessage());
-		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(status).body(err);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 }
